787fb4d34f506f226b540686d09dc6e8464c0f2e
Agri Edd. Oematan <dnr739528@gmail.com>
2021-09-13 15:59:01 +0800

    added nax props on list blog pilihan

--- resources/js/components/public/blog/list/ListBlogPilihanPublic.vue
+++ resources/js/components/public/blog/list/ListBlogPilihanPublic.vue
@@ -12,10 +12,11 @@
 		</div>
 		<div class="d-grid-pilihan-blog px-lg-4">
 			<template v-if="items && items.length">
-				<blog-item-number v-for="(item, index) in items" :index="index" :key="item.id"/>
+				<blog-item-number v-for="(item, index) in items" :index="index + 1" :key="item.id"/>
+				<!-- TODO tambah item placeholder agar memenuhi container -->
 			</template>
 			<template v-else-if="loading">
-				<blog-item-number v-for="(item, index) in items" :index="index" :key="item.id"/>
+				<blog-item-number v-for="(item, index) in 4" :key="index"/>
 			</template>
 			<!-- TODO informasi blog pilihan kosong -->
 			<template v-else>
@@ -33,9 +34,44 @@ import EmptyContent from '../../dummy/EmptyContent.vue'
 import BlogItemNumber from '../item/BlogItemNumber.vue'
 export default {
 	components: { BlogItemNumber, EmptyContent },
+	props: {
+		max: {
+			type: Number,
+			default: 4
+		}
+	},
 	data() {
 		return {
-			items: [],
+			items: [
+				{
+					id: 1,
+					title: 'Blog Pilihan 1',
+					image: 'https://picsum.photos/id/1/200/200',
+					description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+					date: '12/12/2019',
+				},
+				{
+					id: 2,
+					title: 'Blog Pilihan 2',
+					image: 'https://picsum.photos/id/1/200/200',
+					description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+					date: '12/12/2019',
+				},
+				{
+					id: 3,
+					title: 'Blog Pilihan 3',
+					image: 'https://picsum.photos/id/1/200/200',
+					description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+					date: '12/12/2019',
+				},
+				{
+					id: 4,
+					title: 'Blog Pilihan 3',
+					image: 'https://picsum.photos/id/1/200/200',
+					description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+					date: '12/12/2019',
+				}
+			],
 			loading: false,
 		}
 	},
